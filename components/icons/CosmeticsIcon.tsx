c664ba6e04fb94382b2accd2eca770b1a52a9f31
AbouElNour <aboelnour1212@gmail.com>
2025-11-17 14:21:11 +0300

    Updated backend files to fix API handling

---
+++ components/icons/CosmeticsIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CosmeticsIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-full w-full"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={1.5}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5 3l3.057-3L11 3M5 3v5a2 2 0 002 2h10a2 2 0 002-2V3m-14 0h14M12 11v10M9 21h6"
+    />
+  </svg>
+);
+
+export default CosmeticsIcon;
